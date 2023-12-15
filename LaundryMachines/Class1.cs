f055b32b074f5e132d660dc3a5a77db59d35ffa2
Diya Iyer <119765905+diya-iyer@users.noreply.github.com>
2023-12-15 13:21:36 -0500

    Class Library to hold info on machine + settings

---
+++ LaundryMachines/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryMachines
+{
+    public abstract class Machine
+    {
+        public string model;
+        public bool available;
+        public string location;
+        public string loadName;
+        public int loadInterval;
+        public DateTime startTime;
+
+        public void SetStartTime()
+        {
+            this.startTime = DateTime.Now;
+        }
+        
+    }
+    public class WashLoad : Machine
+    {
+        public string washType;
+        public string temperature;
+
+    }
+
+    public class DryLoad : Machine
+    {
+        public string dryType;
+    }
+}
