59c48ff05b541b8e04b6e4eda3dac8135de63466
Tyler Aluko <tyleraluko@gmail.com>
2023-12-18 20:20:58 -0500

    basic history module outline
    history functionality outline code written, main controls added to form designer

--- LaundryApp/Form2.Designer.cs
+++ LaundryApp/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LaundryApp
 {
-    partial class Form2
+    partial class History
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(History));
+            this.navToolStrip = new System.Windows.Forms.ToolStrip();
+            this.homeToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.availToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.historyToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.reportToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.searchPanel = new System.Windows.Forms.Panel();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.historyGroupBox = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.navToolStrip.SuspendLayout();
+            this.searchPanel.SuspendLayout();
+            this.historyGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form2
+            // navToolStrip
+            // 
+            this.navToolStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(104)))), ((int)(((byte)(62)))));
+            this.navToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripButton,
+            this.availToolStripButton,
+            this.historyToolStripButton,
+            this.reportToolStripButton});
+            this.navToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.navToolStrip.Name = "navToolStrip";
+            this.navToolStrip.Size = new System.Drawing.Size(800, 25);
+            this.navToolStrip.TabIndex = 0;
+            this.navToolStrip.Text = "navToolStrip";
+            // 
+            // homeToolStripButton
+            // 
+            this.homeToolStripButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(104)))), ((int)(((byte)(62)))));
+            this.homeToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.homeToolStripButton.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeToolStripButton.ForeColor = System.Drawing.Color.White;
+            this.homeToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.homeToolStripButton.Name = "homeToolStripButton";
+            this.homeToolStripButton.Size = new System.Drawing.Size(43, 22);
+            this.homeToolStripButton.Text = "Home";
+            // 
+            // availToolStripButton
+            // 
+            this.availToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.availToolStripButton.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.availToolStripButton.ForeColor = System.Drawing.Color.White;
+            this.availToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("availToolStripButton.Image")));
+            this.availToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.availToolStripButton.Name = "availToolStripButton";
+            this.availToolStripButton.Size = new System.Drawing.Size(69, 22);
+            this.availToolStripButton.Text = "Availability";
+            // 
+            // historyToolStripButton
+            // 
+            this.historyToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.historyToolStripButton.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.historyToolStripButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.historyToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.historyToolStripButton.Name = "historyToolStripButton";
+            this.historyToolStripButton.Size = new System.Drawing.Size(49, 22);
+            this.historyToolStripButton.Text = "History";
+            // 
+            // reportToolStripButton
+            // 
+            this.reportToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.reportToolStripButton.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportToolStripButton.ForeColor = System.Drawing.Color.White;
+            this.reportToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.reportToolStripButton.Name = "reportToolStripButton";
+            this.reportToolStripButton.Size = new System.Drawing.Size(46, 22);
+            this.reportToolStripButton.Text = "Report";
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(104)))), ((int)(((byte)(62)))));
+            this.searchPanel.Controls.Add(this.searchTextBox);
+            this.searchPanel.Controls.Add(this.searchButton);
+            this.searchPanel.Location = new System.Drawing.Point(12, 39);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(366, 59);
+            this.searchPanel.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(267, 19);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(19, 21);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(242, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // historyGroupBox
+            // 
+            this.historyGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(104)))), ((int)(((byte)(62)))));
+            this.historyGroupBox.Controls.Add(this.dataGridView1);
+            this.historyGroupBox.ForeColor = System.Drawing.Color.White;
+            this.historyGroupBox.Location = new System.Drawing.Point(12, 116);
+            this.historyGroupBox.Name = "historyGroupBox";
+            this.historyGroupBox.Size = new System.Drawing.Size(776, 322);
+            this.historyGroupBox.TabIndex = 2;
+            this.historyGroupBox.TabStop = false;
+            this.historyGroupBox.Text = "History";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // History
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(202)))), ((int)(((byte)(237)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form2";
-            this.Text = "Form2";
+            this.Controls.Add(this.historyGroupBox);
+            this.Controls.Add(this.searchPanel);
+            this.Controls.Add(this.navToolStrip);
+            this.Name = "History";
+            this.Text = "History Log";
+            this.navToolStrip.ResumeLayout(false);
+            this.navToolStrip.PerformLayout();
+            this.searchPanel.ResumeLayout(false);
+            this.searchPanel.PerformLayout();
+            this.historyGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip navToolStrip;
+        private System.Windows.Forms.ToolStripButton homeToolStripButton;
+        private System.Windows.Forms.ToolStripButton availToolStripButton;
+        private System.Windows.Forms.ToolStripButton historyToolStripButton;
+        private System.Windows.Forms.ToolStripButton reportToolStripButton;
+        private System.Windows.Forms.Panel searchPanel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.GroupBox historyGroupBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- LaundryApp/Form2.cs
+++ LaundryApp/Form2.cs
@@ -7,14 +7,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LaundryMachines;
+using Newtonsoft.Json; //for json additions
 
+
+
+/*
+ * Tyler Aluko
+ * IGME.201 - Team 2 Project
+ * The History module allows users to see their past laundry events and save their settings.
+ * Redirects to the Report module.
+ */
 namespace LaundryApp
 {
-    public partial class Form2 : Form
+    
+    
+    public partial class History : Form
     {
-        public Form2()
+
+        //machine info lists
+        private List<WashLoad> washHistory;
+        private List<DryLoad> dryHistory;
+
+        //lists as parameter
+        public History(List<WashLoad> washHistory, List<DryLoad> dryHistory)
         {
+            
             InitializeComponent();
+            
+            this.washHistory = washHistory;
+            this.dryHistory = dryHistory;
+
+            //populate the DataGridView w/ laundry history
+            PopulateHistoryGrid();
+
+            //attach event handlers
+            searchButton.Click += SearchButton_Click;
+            viewReportButton.Click += ViewReportButton_Click;
+
         }
+
+        private void PopulateHistoryGrid()
+        {
+            
+            historyDataGridView.Rows.Clear();
+
+            foreach (WashLoad washLoad in washHistory)
+            {
+                historyDataGridView.Rows.Add("Washer", washLoad.machineName, washLoad.selectedWashType, washLoad.selectedTemperature, washLoad.loadName, washLoad.loadInterval);
+            }
+
+            foreach (DryLoad dryLoad in dryHistory)
+            {
+                historyDataGridView.Rows.Add("Dryer", dryLoad.machineName, dryLoad.selectedDryType, string.Empty, dryLoad.loadName, dryLoad.loadInterval);
+            }
+
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            
+            string searchText = searchTextBox.Text.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(searchText))
+            { //if search box empty
+                //show all history if search text empty
+                PopulateHistoryGrid();
+            }
+            else
+            {
+                //filter history based on search text
+                List<object> filteredHistory = new List<object>();
+
+                foreach (WashLoad washLoad in washHistory)
+                {
+                    if (washLoad.machineName.ToLower().Contains(searchText) ||
+                        washLoad.selectedWashType.ToLower().Contains(searchText) ||
+                        washLoad.selectedTemperature.ToLower().Contains(searchText) ||
+                        washLoad.loadName.ToLower().Contains(searchText) ||
+                        washLoad.loadInterval.ToString().Contains(searchText))
+                    {
+                        filteredHistory.Add(new { Type = "Washer", Load = washLoad });
+                    }
+                }
+
+                foreach (DryLoad dryLoad in dryHistory)
+                {
+                    if (dryLoad.machineName.ToLower().Contains(searchText) ||
+                        dryLoad.selectedDryType.ToLower().Contains(searchText) ||
+                        dryLoad.loadName.ToLower().Contains(searchText) ||
+                        dryLoad.loadInterval.ToString().Contains(searchText))
+                    {
+                        filteredHistory.Add(new { Type = "Dryer", Load = dryLoad });
+                    }
+                }
+
+                //update DataGridView w/ filtered results
+                UpdateHistoryGrid(filteredHistory);
+
+            }
+
+        }
+
+        private void UpdateHistoryGrid(List<object> filteredHistory)
+        {
+            
+            historyDataGridView.Rows.Clear();
+
+            foreach (object entry in filteredHistory)
+            {
+                if (entry is { Type: "Washer", Load: WashLoad washLoad })
+                {
+                    historyDataGridView.Rows.Add("Washer", washLoad.machineName, washLoad.selectedWashType, washLoad.selectedTemperature, washLoad.loadName, washLoad.loadInterval);
+                }
+                else if (entry is { Type: "Dryer", Load: DryLoad dryLoad })
+                {
+                    historyDataGridView.Rows.Add("Dryer", dryLoad.machineName, dryLoad.selectedDryType, string.Empty, dryLoad.loadName, dryLoad.loadInterval);
+                }
+            }
+
+        }
+
+        //i'm thinking that instead reports should accept a list from the history log that should be used, as navigating to reports from this page seems redundant
+        private void ViewReportButton_Click(object sender, EventArgs e)
+        {
+            //navigate to report page
+            Report reportForm = new Report(washHistory, dryHistory);
+            reportForm.Show();
+        }
+
     }
+
+
 }
